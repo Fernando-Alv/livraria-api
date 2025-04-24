855b43d0c0ab0c2ff5de488c377e6483f3795b71
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-04-24 16:20:14 -0300

    commit inicial

---
+++ src/test/java/com/meusprojetos/livraria/api/LivrariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meusprojetos.livraria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
