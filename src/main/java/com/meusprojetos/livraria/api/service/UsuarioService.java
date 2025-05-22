7e94aff446c03f9be2d49301879cd7cb032a8034
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-05-22 16:46:10 -0300

    refactor: usa UsuarioMapper para retornar DTO na consulta de usuário

--- src/main/java/com/meusprojetos/livraria/api/service/UsuarioService.java
+++ src/main/java/com/meusprojetos/livraria/api/service/UsuarioService.java
@@ -1,6 +1,10 @@
 package com.meusprojetos.livraria.api.service;
 
 import java.util.List;
+
+import com.meusprojetos.livraria.api.dto.UsuarioEmprestimoResponseDTO;
+import com.meusprojetos.livraria.api.dto.UsuarioResponseDTO;
+import com.meusprojetos.livraria.api.mapstruct.UsuarioMapper;
 import org.springframework.stereotype.Service;
 import com.meusprojetos.livraria.api.entity.Usuario;
 import com.meusprojetos.livraria.api.exception.RecursoNaoEncontradoException;
@@ -13,19 +17,24 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioService {
 
 	private final UsuarioRepository usuarioRepository;
+
+	private final UsuarioMapper usuarioMapper;
 	
 	public Usuario inserirUsuario(Usuario usuario) {
 		return usuarioRepository.save(usuario);
 	}
 	
-	public List<Usuario> consultarUsuarios() {
-		return usuarioRepository.findAll();
+	public List<UsuarioResponseDTO> consultarUsuarios() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+
+		return usuarioMapper.paraUsuarioResponseDTO(usuarios);
 	}
 	
-	public Usuario consultarUsuarioPorId(Long id) {
-		
-		return usuarioRepository.findById(id)
+	public UsuarioEmprestimoResponseDTO consultarUsuarioPorId(Long id) {
+		Usuario usuario = usuarioRepository.findById(id)
 				.orElseThrow(() -> new RecursoNaoEncontradoException("Usuário não encontrado com ID: " + id));
+
+		return usuarioMapper.paraUsuarioEmprestimoResponseDTO(usuario);
 	}
 	
 	public Usuario atualizarUsuario(Long id, Usuario usuarioAtualizado) {
