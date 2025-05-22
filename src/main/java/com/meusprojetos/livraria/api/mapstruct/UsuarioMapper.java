c88e9e6053af50717d78b70a04ad30987430783c
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-05-22 16:41:27 -0300

    feat: adiciona UsuarioMapper para conversão de entidade para DTO

---
+++ src/main/java/com/meusprojetos/livraria/api/mapstruct/UsuarioMapper.java
@@ -0,0 +1,16 @@
+package com.meusprojetos.livraria.api.mapstruct;
+
+import com.meusprojetos.livraria.api.dto.UsuarioEmprestimoResponseDTO;
+import com.meusprojetos.livraria.api.dto.UsuarioResponseDTO;
+import com.meusprojetos.livraria.api.entity.Usuario;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = EmprestimoMapper.class)
+public interface UsuarioMapper {
+
+    UsuarioEmprestimoResponseDTO paraUsuarioEmprestimoResponseDTO(Usuario usuario);
+
+    List<UsuarioResponseDTO> paraUsuarioResponseDTO(List<Usuario> usuarios);
+}
