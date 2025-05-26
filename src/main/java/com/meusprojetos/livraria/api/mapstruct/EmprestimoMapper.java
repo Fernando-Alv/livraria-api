4bf8bfa908ab3677c89150c6e8af31e7d7c9c184
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-05-25 22:42:10 -0300

    feat: adiciona mapeamento de Emprestimo para EmprestimoResponseDTO

--- src/main/java/com/meusprojetos/livraria/api/mapstruct/EmprestimoMapper.java
+++ src/main/java/com/meusprojetos/livraria/api/mapstruct/EmprestimoMapper.java
@@ -1,9 +1,12 @@
 package com.meusprojetos.livraria.api.mapstruct;
 
+import com.meusprojetos.livraria.api.dto.EmprestimoResponseDTO;
 import com.meusprojetos.livraria.api.dto.EmprestimoResumoDTO;
 import com.meusprojetos.livraria.api.entity.Emprestimo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EmprestimoMapper {
@@ -11,4 +14,12 @@ public interface EmprestimoMapper {
     @Mapping(source = "livro.id", target = "idLivro")
     @Mapping(source = "livro.titulo", target = "tituloLivro")
     EmprestimoResumoDTO paraEmprestimoResumoDTO(Emprestimo emprestimo);
+
+    @Mapping(source = "livro.id", target = "idLivro")
+    @Mapping(source = "livro.titulo", target = "tituloLivro")
+    @Mapping(source = "usuario.id", target = "idUsuario")
+    @Mapping(source = "usuario.nome", target = "nomeUsuario")
+    EmprestimoResponseDTO paraEmprestimoResponseDTO(Emprestimo emprestimo);
+
+    List<EmprestimoResponseDTO> paraListaEmprestimosResponseDTO(List<Emprestimo> emprestimos);
 }
